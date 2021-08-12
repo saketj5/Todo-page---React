547648f3360d8f540efe7d8dbd45e0e0c2a35867
saketj5 <58130182+saketj5@users.noreply.github.com>
2021-08-12 12:55:17 +1000

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+import Todo from "./components/Todo";
+// import Modal from "./components/Modal";
+// import Backdrop from "./components/Backdrop";
+
+function App() {
+  return (
+    <div>
+      <h1>My Todos</h1>
+      <Todo text = "Basic React"/>
+      <Todo text = "Intermediate React"/>
+      <Todo text="Advance React" />
+      
+    </div> 
+  );
+}
+
+export default App;
